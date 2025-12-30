aec2662971d9558ee8fe04e23383bba6428a178f
mossser$ <kanyamosesm@gmail.com>
2025-12-30 18:41:50 +0000

    Add routing for case study components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Portfolio from './components/portfolio.jsx'
+import FilmSlateCase from './components/case-studies/FilmSlateCase.jsx'
+import FreqSpaceCase from './components/case-studies/FreqSpaceCase.jsx'
+import PublicationSummarizerCase from './components/case-studies/PublicationSummarizerCase.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Portfolio />
+        <Router>
+            <Routes>
+                <Route path="/" element={<Portfolio />} />
+                <Route path="/case-study/filmslate" element={<FilmSlateCase />} />
+                <Route path="/case-study/freq-space" element={<FreqSpaceCase />} />
+                <Route path="/case-study/publication-summarizer" element={<PublicationSummarizerCase />} />
+            </Routes>
+        </Router>
     </React.StrictMode>,
 )
